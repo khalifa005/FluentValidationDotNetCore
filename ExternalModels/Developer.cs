055795ff9fd048b01deeeb7c9d5f4bdd9399d750
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-10-14 09:51:32 +0200

    Add prop to developer

--- ExternalModels/Developer.cs
+++ ExternalModels/Developer.cs
@@ -4,5 +4,10 @@ namespace ExternalModels
 {
     public class Developer
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public decimal Experience { get; set; }
+
     }
 }
