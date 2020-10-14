650b17ce2f06d7a656e47d2717812407bafd05a0
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-10-14 09:51:50 +0200

    Building first custom validation

---
+++ FluentValidationDotNetCore/Controllers/DeveloperController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExternalModels;
+
+namespace FluentValidationDotNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeveloperController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create(Developer developer)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ FluentValidationDotNetCore/Validators/DeveloperValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExternalModels;
+using FluentValidation;
+
+namespace FluentValidationDotNetCore.Validators
+{
+    public class DeveloperValidator : AbstractValidator<Developer>
+    {
+        public DeveloperValidator()
+        {
+            RuleFor(X => X.FirstName).NotEmpty().WithMessage("required_first_name");
+        }
+    }
+}
