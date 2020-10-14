939263b71651ad128302107fdc6f6629e89e2c3e
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-10-14 10:23:06 +0200

    Add more validation rule

--- FluentValidationDotNetCore/Validators/DeveloperValidator.cs
+++ FluentValidationDotNetCore/Validators/DeveloperValidator.cs
@@ -9,9 +9,19 @@ namespace FluentValidationDotNetCore.Validators
 {
     public class DeveloperValidator : AbstractValidator<Developer>
     {
+        private bool IsValidName(string name)
+        {
+            return name.All(Char.IsLetter);
+        }
         public DeveloperValidator()
         {
-            RuleFor(X => X.FirstName).NotEmpty().WithMessage("required_first_name");
+            RuleFor(X => X.FirstName)
+                .NotEmpty()
+                .WithMessage("required_first_name")
+                .Length(2,10).WithMessage("first_name_length_should_be_between_2_and_10")
+                .Must(x=>x.All(char.IsLetter)).WithMessage("first_name_should_be_characters_only");
+
+            RuleFor(x=>x.Email).EmailAddress()
         }
     }
 }
