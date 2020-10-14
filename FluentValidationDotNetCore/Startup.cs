93e655738ebc65d461f441f50875493d1e7e26d9
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-10-14 09:14:58 +0200

    Configure fluent validation package

--- FluentValidationDotNetCore/Startup.cs
+++ FluentValidationDotNetCore/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation.AspNetCore;
 
 namespace FluentValidationDotNetCore
 {
@@ -25,7 +26,13 @@ namespace FluentValidationDotNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+
+            services.AddControllers()
+                .AddFluentValidation(f =>
+                {
+                    f.RegisterValidatorsFromAssemblyContaining<Startup>();
+                    f.RunDefaultMvcValidationAfterFluentValidationExecutes = false;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
